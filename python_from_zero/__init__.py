9dd63a0a8221296b4ab62f07af44becd41960e4e
Anzorjon <murmullo00@mail.ru>
2021-06-02 21:40:58 +0500

    first commit

---
+++ python_from_zero/__init__.py
@@ -0,0 +1,15 @@
+name = 'Anzor'
+age = 25
+family = ['Madin', 'Logic', 'Magic']
+
+print(f'type of name: {type(name)}')
+print(f'type of age: {type(age)}')
+print(f'id of family {id(family)}')
+
+import keyword
+
+kw_list = keyword.kwlist
+# print(kw_list)
+
+for kw in kw_list:
+    print(kw)
